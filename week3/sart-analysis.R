7128115477ada8c278bfd2ccd301918d89101bc7
shildone <78596333+shildone@users.noreply.github.com>
2021-02-22 23:31:16 +0100

    Attend plot + R code for Q3b

---
+++ week3/sart-analysis.R
@@ -0,0 +1,78 @@
+
+# Read in the data
+data_path <- "C:/Users/shild/Documents/AI/Jaar 2/Blok 3/Cognitive Modelling Practical/week 3/" # Change to the location of your output folder
+
+library(stringr)
+library(data.table)
+
+# Read the trace file into memory
+trace <- file(paste0(data_path, "sart-trace.txt"), "r")
+lines <- readLines(trace)
+
+activations_at <- data.table(time = rep(0, 500), activation = rep(0, 500)) # Preallocate a data.table where we can store ATTEND activation values
+activations_wa <- data.table(time = rep(0, 500), activation = rep(0, 500)) # Preallocate a data.table where we can store WANDER activation values
+
+actpersec_at <- 0
+actpersec_wa <- 0
+activationtotal_at <- 0
+activationtotal_wa <- 0
+time <- 0
+
+
+for(i in 4:length(lines)){
+
+  # Read a single line
+  line <- lines[i]
+  
+  if(!str_detect(line, "Chunk")){
+    if(str_detect(line, "\\d+")){
+      current_time <- as.numeric(str_extract(line, "\\d+"))
+      while(current_time != time){
+        if(actpersec_at == 0){
+          meanact_at <- 0
+        }else {
+          meanact_at <- activationtotal_at/actpersec_at
+        }
+        if(actpersec_wa == 0){
+          meanact_wa <- 0
+        }else {
+          meanact_wa <- activationtotal_wa/actpersec_wa
+        }
+        activations_at$time[time] <- time
+        activations_at$activation[time] <- meanact_at
+        activations_wa$time[time] <- time
+        activations_wa$activation[time] <- meanact_wa
+        time <- time + 1
+        activationtotal_at <- 0
+        actpersec_at <- 0
+        activationtotal_wa <- 0
+        actpersec_wa <- 0
+      }
+    } 
+  }
+  
+  else if(str_detect(line, "Chunk ATTEND has an activation of:")) {
+    actpersec_at <- actpersec_at+1
+    y <- str_extract(line, "[:digit:][:punct:]\\d+")
+    activationtotal_at <- activationtotal_at + as.numeric(y)
+  }
+  else if(str_detect(line, "Chunk WANDER has an activation of:")) {
+    actpersec_wa <- actpersec_wa+1
+    y <- str_extract(line, "[:digit:][:punct:]\\d+")
+    activationtotal_wa <- activationtotal_wa + as.numeric(y)
+  }
+}
+
+
+plot <- ggplot()
+plot + geom_line(activations_at, mapping = aes(x=time, y=activation), color='darkgreen') + 
+  geom_line(activations_wa, mapping = aes(x=time, y=activation), color='darkblue') +
+  xlim(1,500) + 
+  ylim(4.5,6) + 
+  ggtitle("Activation of chunk ATTEND and chunk WANDER over time") +
+  labs(x = "Time (s)", y = "Mean activation") +
+  theme_bw() + 
+  theme(plot.title=element_text(size=20, face="bold")) +
+  theme(axis.text=element_text(size=14),
+        axis.title=element_text(size=18,face="bold"))
+  
